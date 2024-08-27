cfbe8d73b8f295e3493b390b40f316c625546a8f
Tarvo R. <TarvoReinpalu@gmail.com>
2024-08-27 22:10:35 +0300

    WIP

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,118 +1,150 @@
 <template>
-    <DefaultField :field="currentField" :errors="errors" :show-help-text="showHelpText" :index="index"
-        :id="field.attribute" :full-width-content="fullWidthContent">
-        <template #field>
-            <a ref="emojianchor"></a>
-            <textarea ref="editor" :id="field.attribute" :class="errorClasses" class="w-full" style="height:200px"
-                :value="value" />
-        </template>
-    </DefaultField>
+  <DefaultField
+    :field="currentField"
+    :errors="errors"
+    :show-help-text="showHelpText"
+    :index="index"
+    :id="field.attribute"
+    :full-width-content="fullWidthContent"
+  >
+    <template #field>
+      <a ref="emojianchor"></a>
+      <textarea
+        ref="editor"
+        :id="field.attribute"
+        :class="errorClasses"
+        class="w-full"
+        style="height: 200px"
+        :value="value"
+      />
+    </template>
+  </DefaultField>
 </template>
 
 <script>
-import { DependentFormField, HandlesValidationErrors } from 'laravel-nova'
+  import { DependentFormField, HandlesValidationErrors } from "laravel-nova";
 
-import suneditor from 'suneditor'
-import plugins from 'suneditor/src/plugins'
+  import suneditor from "suneditor";
+  import plugins from "suneditor/src/plugins";
 
-import { picmo } from 'suneditor-picmo-emoji'
+  import { picmo } from "suneditor-picmo-emoji";
 
-export default {
+  export default {
     mixins: [DependentFormField, HandlesValidationErrors],
-    emits: ['field-changed'],
+    emits: ["field-changed"],
 
-    props: ['resourceName', 'resourceId', 'field'],
+    props: ["resourceName", "resourceId", "field"],
     data() {
-        return {
-            mounted: false,
-            index: 0,
-        }
+      return {
+        mounted: false,
+        index: 0,
+      };
     },
 
     watch: {
-        currentlyIsVisible(current, previous) {
-            if (current === true && previous === false) {
-                this.$nextTick(() => this.handleShowingComponent())
-            } else if (current === false && previous === true) {
-                this.handleHidingComponent()
-            }
-        },
+      currentlyIsVisible(current, previous) {
+        if (current === true && previous === false) {
+          this.$nextTick(() => this.handleShowingComponent());
+        } else if (current === false && previous === true) {
+          this.handleHidingComponent();
+        }
+      },
     },
 
     created() {
-        this.setInitialValue()
+      this.setInitialValue();
     },
 
     mounted() {
-        Nova.$on(this.fieldAttributeValueEventName, this.listenToValueChanges)
+      Nova.$on(this.fieldAttributeValueEventName, this.listenToValueChanges);
 
-        if (this.isVisible) {
-            this.handleShowingComponent();
-        }
+      if (this.isVisible) {
+        this.handleShowingComponent();
+      }
     },
 
     beforeUnmount() {
-        Nova.$off(this.fieldAttributeValueEventName, this.listenToValueChanges)
+      Nova.$off(this.fieldAttributeValueEventName, this.listenToValueChanges);
     },
 
     methods: {
-        initEditor() {
-            const buttonListName = this.field.buttonListName ?? 'default';
-            const buttonList = this.field.buttonList ?? Nova.config(`suneditor-buttonLists`)[buttonListName];
-            const settings = this.field.settings ?? Nova.config(`suneditor-settings`);
+      initEditor() {
+        const buttonListName = this.field.buttonListName ?? "default";
+        const buttonList =
+          this.field.buttonList ??
+          Nova.config(`suneditor-buttonLists`)[buttonListName];
+        const settings =
+          this.field.settings ?? Nova.config(`suneditor-settings`);
 
-            this.editor = suneditor.create(this.$refs.editor, {
-                plugins: {
-                    picmo,
-                    ...plugins
-                },
-                imageUploadUrl: `/nova-api/webard/nova-suneditor/${this.resourceName}/field-attachment/${this.fieldAttribute}`,
-                buttonList,
-                ...settings,
-            });
+        this.editor = suneditor.create(this.$refs.editor, {
+          plugins: {
+            picmo,
+            ...plugins,
+          },
+          imageUploadUrl: `/nova-api/webard/nova-suneditor/${this.resourceName}/field-attachment/${this.fieldAttribute}`,
+          buttonList,
+          pasteTagsBlacklist: "span|div",
+          ...settings,
+        });
 
+        this.editor.onPaste = (e, cleanData, maxCharCount, core) => {
+          // Add padding before and after tags
+          return this.addSpacesAroundTags(cleanData);
+        };
+      },
 
+      addSpacesAroundTags(html) {
+        // Add space before opening <a> and <span> tags
+        html = html.replace(/(?<! )(<(a|span)[^>]*>)/g, " $1");
 
-        },
+        // Add space after closing </a> and </span> tags
+        html = html.replace(/(<\/(a|span)>)(?! )/g, "$1 ");
 
-        destroyEditor() {
-            // this.editor.destroy();
-        },
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-            this.value = this.field.value || ''
-        },
+        return html;
+      },
 
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        handleChange(value) {
-            if (this.isVisible) {
-                this.editor.setContents(value)
+      destroyEditor() {
+        // this.editor.destroy();
+      },
+      /*
+       * Set the initial, internal value for the field.
+       */
+      setInitialValue() {
+        this.value = this.field.value || "";
+      },
 
-                this.$emit('field-changed')
-            }
-        },
+      /**
+       * Fill the given FormData object with the field's internal value.
+       */
+      handleChange(value) {
+        if (this.isVisible) {
+          this.editor.setContents(value);
 
-        fill(formData) {
-            this.fillIfVisible(formData, this.fieldAttribute, this.editor.getContents() || '')
-        },
+          this.$emit("field-changed");
+        }
+      },
 
-        onSyncedField() {
-            this.handleChange(this.currentField.value ?? this.value)
-            this.index++
-        },
+      fill(formData) {
+        this.fillIfVisible(
+          formData,
+          this.fieldAttribute,
+          this.editor.getContents() || ""
+        );
+      },
 
-        handleShowingComponent() {
-            this.initEditor();
-            this.handleChange(this.currentField.value ?? this.value)
-        },
+      onSyncedField() {
+        this.handleChange(this.currentField.value ?? this.value);
+        this.index++;
+      },
 
-        handleHidingComponent() {
-            this.destroyEditor()
-        },
+      handleShowingComponent() {
+        this.initEditor();
+        this.handleChange(this.currentField.value ?? this.value);
+      },
+
+      handleHidingComponent() {
+        this.destroyEditor();
+      },
     },
-}
+  };
 </script>
